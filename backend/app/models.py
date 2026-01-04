c07fae463341d617fab91a163d4706c8d94733c0
FjeldMats <FjeldMats@gmail.com>
2026-01-04 21:42:41 +0100

    fix: Add email field to User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -35,6 +35,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
     def set_password(self, password):
