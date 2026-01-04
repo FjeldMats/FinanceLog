cc5f439d70b1c3d94374a6534cc8fad940e472e5
FjeldMats <FjeldMats@gmail.com>
2026-01-04 21:49:30 +0100

    fix: Add JWT_ALGORITHM to config

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+    JWT_ALGORITHM = os.getenv('JWT_ALGORITHM', 'HS256')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES_HOURS', '24')))
